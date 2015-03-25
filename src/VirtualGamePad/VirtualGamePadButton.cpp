5bbc9c09632db0a16b2aa369200faafe8df7d4e8
loki666 <none@none>
2015-03-25 13:38:47 +0100

    VirtualGamePad: fire OnPressed and OnReleased track

--- src/VirtualGamePad/VirtualGamePadButton.cpp
+++ src/VirtualGamePad/VirtualGamePadButton.cpp
@@ -7,16 +7,32 @@
 
 #include "VirtualGamePadButton.h"
 
-static const orxSTRING szConfigInputName = "InputName";
+static  const orxSTRING szConfigInputName       = "InputName";
+static  const orxSTRING szConfigOnPressedTrack  = "ButtonOnPressedTrack";
+static  const orxSTRING szConfigOnReleasedTrack = "ButtonOnReleasedTrack";
 
 void VirtualGamePadButton::ProcessTouch(orxVECTOR *_pvPosition)
 {
   orxInput_SetPermanentValue(mzInputName, orxFLOAT_1);
+
+  PushConfigSection();
+  if(orxConfig_HasValue(szConfigOnPressedTrack))
+  {
+    orxObject_AddTimeLineTrack(GetOrxObject(), orxConfig_GetString(szConfigOnPressedTrack));
+  }
+  PopConfigSection();
 }
 
 void VirtualGamePadButton::Reset()
 {
   orxInput_ResetValue(mzInputName);
+
+  PushConfigSection();
+  if(orxConfig_HasValue(szConfigOnReleasedTrack))
+  {
+    orxObject_AddTimeLineTrack(GetOrxObject(), orxConfig_GetString(szConfigOnReleasedTrack));
+  }
+  PopConfigSection();
 }
 
 void VirtualGamePadButton::OnCreate()
